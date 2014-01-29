b35334b467cbcb14b07eff17dbb5ccddcd0a1f33
Richard Hattersley <rhattersley@gmail.com>
2014-01-29 13:30:36 +0000

    Generalise axis handling and improve docstrings.

---
+++ biggus/tests/unit/test__normalise_axis.py
@@ -0,0 +1,83 @@
+# (C) British Crown Copyright 2014, Met Office
+#
+# This file is part of Biggus.
+#
+# Biggus is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Biggus is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Biggus. If not, see <http://www.gnu.org/licenses/>.
+"""Unit tests for `biggus._normalise_axis`."""
+
+import unittest
+
+from biggus import _normalise_axis
+
+
+class TestNone(unittest.TestCase):
+    def test(self):
+        self.assertIs(_normalise_axis(None), None)
+
+
+class TestValid(unittest.TestCase):
+    def check(self, argument, expected):
+        result = _normalise_axis(argument)
+        self.assertEqual(result, expected)
+        self.assertIsInstance(result, tuple)
+
+    def test_zero(self):
+        self.check(0, (0,))
+
+    def test_one(self):
+        self.check(1, (1,))
+
+    def test_single_tuple(self):
+        self.check((8,), (8,))
+
+    def test_single_list(self):
+        self.check([8], (8,))
+
+    def test_multi_tuple(self):
+        self.check((3, 2), (3, 2))
+
+    def test_multi_list(self):
+        self.check([4, 1], (4, 1))
+
+
+class TestInvalidInt(unittest.TestCase):
+    def test_builtin(self):
+        with self.assertRaises(TypeError):
+            _normalise_axis(open)
+
+    def test_dict(self):
+        with self.assertRaises(TypeError):
+            _normalise_axis({})
+
+    def test_float(self):
+        with self.assertRaises(TypeError):
+            _normalise_axis(2.3)
+
+    def test_str(self):
+        with self.assertRaises(TypeError):
+            _normalise_axis('23')
+
+
+class TestInvalidIterable(unittest.TestCase):
+    def test_mixed_tuple(self):
+        with self.assertRaises(TypeError):
+            _normalise_axis((2, '23', 6))
+
+    def test_all_bad_tuple(self):
+        with self.assertRaises(TypeError):
+            _normalise_axis((2.8, '23', {6: 3}))
+
+
+if __name__ == '__main__':
+    unittest.main()
