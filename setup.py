e281f2cceafcba21723a923e180116da9ac5b439
Richard Hattersley <rhattersley@gmail.com>
2014-06-17 16:46:25 +0100

    Add clean_source command to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,34 @@
-from setuptools import setup
+from __future__ import print_function
+
+import os
+
+from setuptools import setup, Command
+
+
+class CleanSource(Command):
+    """
+    Removes orphaned pyc/pyo files from the sources.
+
+    """
+    description = 'clean orphaned pyc/pyo files from sources'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        for root_path, dir_names, file_names in os.walk('biggus'):
+            for file_name in file_names:
+                if file_name.endswith('pyc') or file_name.endswith('pyo'):
+                    compiled_path = os.path.join(root_path, file_name)
+                    source_path = compiled_path[:-1]
+                    if not os.path.exists(source_path):
+                        print('Cleaning', compiled_path)
+                        os.remove(compiled_path)
+
 
 setup(
     name='Biggus',
@@ -17,5 +47,6 @@ setup(
     description='Virtual large arrays and lazy evaluation',
     long_description=open('README.rst').read(),
     use_2to3=True,
-    test_suite='biggus.tests'
+    test_suite='biggus.tests',
+    cmdclass={'clean_source': CleanSource}
 )
